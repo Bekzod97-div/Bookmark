636b5b5807e7046cd06452f57f5a9cc6036b683d
Bekzod97-div <xushmatofbekzod@gmail.com>
2023-02-23 10:17:13 +0500

    add hover to faq btn and modify some styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 document.getElementById("first-btn").addEventListener("click", function() {
-    document.getElementById("first").style.display = "block";
-    document.getElementById("second").style.display = "none";
-    document.getElementById("third").style.display = "none";
-  });
-  document.getElementById("second-btn").addEventListener("click", function() {
-    document.getElementById("first").style.display = "none";
-    document.getElementById("second").style.display = "block";
-    document.getElementById("third").style.display = "none";
-  });
-  document.getElementById("third-btn").addEventListener("click", function() {
-    document.getElementById("first").style.display = "none";
-    document.getElementById("second").style.display = "none";
-    document.getElementById("third").style.display = "block";
-  });
+  document.getElementById("first").style.display = "block";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "none";
+});
+document.getElementById("second-btn").addEventListener("click", function() {
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "block";
+  document.getElementById("third").style.display = "none";
+});
+document.getElementById("third-btn").addEventListener("click", function() {
+  document.getElementById("first").style.display = "none";
+  document.getElementById("second").style.display = "none";
+  document.getElementById("third").style.display = "block";
+});
+
+  // FAQ
+  const elsButton = document.querySelectorAll('.faq__btn');
+  const elsDescription = document.querySelectorAll('.faq__btn__description');
+  const elsIcon = document.querySelectorAll('.faq-svg');
+
+  elsButton.forEach(function (elsButton, index) {
+    elsButton.addEventListener('click', function() {
+      elsDescription[index].classList.toggle('js__faq__btn__description');
+      elsIcon[index].classList.toggle('rotate');
+    });
+  })
