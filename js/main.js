e860325a572fba5a3ed52de3aebe417bb3598859
Bekzod97-div <xushmatofbekzod@gmail.com>
2023-02-25 00:00:57 +0500

    make features tabs work with js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 // Features
-const elsTabItem = document.querySelectorAll('.js-featuresnav__item');
+const elsTabItem = document.querySelectorAll('.featuresnav__item');
 const elsLink = document.querySelectorAll('.js-featuresnav__link');
+const elsFeaturesInfo = document.querySelectorAll('.features__info__container');
+
+function deactivateTabItems() {
+  elsTabItem.forEach(function (elTabItem) {
+    elTabItem.classList.remove('js-featuresnav__item');
+  });
+};
+
+function deactivateFeaturesInfo() {
+  elsFeaturesInfo.forEach(function (elFeaturesInfo) {
+    elFeaturesInfo.classList.remove('js-features__info__container');
+  });
+};
 
 elsLink.forEach(function (elLink) {
   elLink.addEventListener('click', function (evt) {
@@ -8,33 +21,22 @@ elsLink.forEach(function (elLink) {
     evt.preventDefault();
 
     //Remove js-featuresnav__item class
-    elsTabItem.forEach(function (elTabItem) {
-      elTabItem.classList.remove('js-featuresnav__item');
-    });
+     deactivateTabItems();
+
+    //  Remove Js_features__info__container from links
+    deactivateFeaturesInfo();
+
+    // Show clicked panel
+    // const elActiveFeaturesInfo = document.querySelector(`#${elLink.href.split('#')[1]}`);
+    const elActiveFeaturesInfo = document.querySelector(elLink.dataset.tab);
+    elActiveFeaturesInfo.classList.add('js-features__info__container');
+
 
     // Add active class to featuresnav__item classlist
     elLink.parentElement.classList.add('js-featuresnav__item');
-
-    console.log(elLink.href.split('#'));
   })
 })
 
-
-// document.getElementById("first-btn").addEventListener("click", function() {
-//   document.getElementById("first").style.display = "block";
-//   document.getElementById("second").style.display = "none";
-//   document.getElementById("third").style.display = "none";
-// });
-// document.getElementById("second-btn").addEventListener("click", function() {
-//   document.getElementById("first").style.display = "none";
-//   document.getElementById("second").style.display = "block";
-//   document.getElementById("third").style.display = "none";
-// });
-// document.getElementById("third-btn").addEventListener("click", function() {
-//   document.getElementById("first").style.display = "none";
-//   document.getElementById("second").style.display = "none";
-//   document.getElementById("third").style.display = "block";
-// });
 
   // FAQ
   const elsButton = document.querySelectorAll('.faq__btn');
